32963c4cd185b87d6c51d4ff737d58f6e52c2003
moundu <munenemuondu@gmail.com>
2020-08-02 12:22:59 +0300

    I have made some changes

---
+++ how_to_create_a_calendar.py
@@ -0,0 +1,8 @@
+import calendar as cal
+
+#print(cal.isleap(2010))
+year = int(input("Enter the year you want to see:  "))
+month = int(input("Enter the month you want to see:  "))
+print(cal.month(year,month))
+#year = int(input("Enter the year you want to see:  "))
+#print(cal.calendar(year))
